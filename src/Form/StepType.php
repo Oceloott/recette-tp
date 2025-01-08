86f514499e463a69c88e03128cf40fecc0462f9d
Oceloott <60733546+Oceloott@users.noreply.github.com>
2025-01-08 12:28:37 +0100

    crud

---
+++ src/Form/StepType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Recipe;
+use App\Entity\Step;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StepType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('description')
+            ->add('stepOrder')
+            ->add('recipe', EntityType::class, [
+                'class' => Recipe::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Step::class,
+        ]);
+    }
+}
